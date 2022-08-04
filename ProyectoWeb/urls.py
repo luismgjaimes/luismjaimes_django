c8da4b975bad06c714b1b03a7f4effbdaf29117c
luismjaimes <luismjaimes@gmail.com>
2022-08-03 19:48:36 -0500

    respaldo inicio blog

--- ProyectoWeb/urls.py
+++ ProyectoWeb/urls.py
@@ -21,7 +21,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-     path('servicios/', include('servicios.urls')),
+    path('servicios/', include('servicios.urls')),
+
+    path('blog/', include('blog.urls')),
 
     path('', include('ProyectoWebApp.urls')),
 

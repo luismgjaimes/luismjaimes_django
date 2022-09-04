da1e865197444f1b4aa043884a47fa7784e23567
luismjaimes <luismjaimes@gmail.com>
2022-09-04 13:43:50 -0500

    octavo respaldo

--- contacto/forms.py
+++ contacto/forms.py
@@ -7,6 +7,6 @@ class FormularioContacto(forms.Form):
 
     email=forms.CharField(label="EmailNombre", required=True)
 
-    contenido=forms.CharField(label="Contenido")
+    contenido=forms.CharField(label="Contenido", widget=forms.Textarea)
 
 
